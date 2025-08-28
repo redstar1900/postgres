aaea65fee535516a5f6741e022fbf9b01dca4e2a
redstar1900 <redstardb1900@163.com>
2025-08-28 16:19:12 +0000

    interrupt.h

--- src/include/postmaster/interrupt.h
+++ src/include/postmaster/interrupt.h
@@ -1,17 +1,16 @@
 /*-------------------------------------------------------------------------
  *
  * interrupt.h
- *	  Interrupt handling routines.
+ *    中断处理相关函数声明。
  *
- * Responses to interrupts are fairly varied and many types of backends
- * have their own implementations, but we provide a few generic things
- * here to facilitate code reuse.
+ * 对中断的响应方式多种多样，许多类型的后端进程有自己的实现，
+ * 但这里我们提供了一些通用的内容以便代码复用。
  *
  * Portions Copyright (c) 1996-2024, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  src/include/postmaster/interrupt.h
+ *    src/include/postmaster/interrupt.h
  *
  *-------------------------------------------------------------------------
  */
@@ -21,12 +20,18 @@
 
 #include <signal.h>
 
+// 标记是否有配置重载请求待处理
 extern PGDLLIMPORT volatile sig_atomic_t ConfigReloadPending;
+// 标记是否有关闭请求待处理
 extern PGDLLIMPORT volatile sig_atomic_t ShutdownRequestPending;
 
+// 处理主循环中的中断
 extern void HandleMainLoopInterrupts(void);
+// 配置重载信号处理函数
 extern void SignalHandlerForConfigReload(SIGNAL_ARGS);
+// 崩溃退出信号处理函数
 extern void SignalHandlerForCrashExit(SIGNAL_ARGS);
+// 关闭请求信号处理函数
 extern void SignalHandlerForShutdownRequest(SIGNAL_ARGS);
 
 #endif
